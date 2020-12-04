9f5dddddeccead118fc2ccb79c9a15b66cca1485
Vish Singh <vishalicious213@gmail.com>
2020-12-03 16:48:12 -0800

    added 8

--- src/demonstration_08.py
+++ src/demonstration_08.py
@@ -10,6 +10,16 @@ Examples:
 - num_args(True, False) ➞ 2
 - num_args({}) ➞ 1
 """
-def num_args():
+def num_args(*args, arg2):
     # Your code here
+    print(len(args))
+    print(arg2)
+    return
 
+num_args("foo", "bar", arg2='this is a keyword arg')
+# num_args("foo")
+
+# the arguments in the parenthesis in a specific location are called "positional arguments"
+# named/keyword-only arguments look like props. They're key/value pairs.
+# adding an asterisk in the parenthesis for a function lets it take unlimited arguments
+# kwargs = keyword arguments
